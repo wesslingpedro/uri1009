ff97cdcf57b01321cf83cf85d14e1871316c107a
wesslingpedro <wesslingpedro@gmail.com>
2024-05-08 20:00:44 -0300

    Primeiro commit

---
+++ uri1009/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1009{
+    class Program {
+    static void Main(string[] args){
+
+            //total de vendas - 15% = resultado + salario fixo
+
+            string nome;
+            double salariof, vendas, salariot;
+
+         
+            nome = Console.ReadLine();
+            salariof = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            salariot = vendas * 15.0 / 100 + salariof;
+
+
+
+
+
+            Console.WriteLine("TOTAL = R$ " + salariot.ToString("F2", CultureInfo.InvariantCulture));
+
+            
+
+
+
+
+        }
+    
+    }
+
+}
